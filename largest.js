8335aa3f754e0ba90ad2d1557895aa49f7714f56
vinay <vinayvarma2545@gmail.com>
2022-09-21 18:52:05 +0530

    largest

--- largest.js
+++ largest.js
@@ -1,15 +1,23 @@
-const x = 7;
-const y = 1;
-const z = 55;
+// const x = 7;
+// const y = 1;
+// const z = 55;
 
-if (x>y && x>z){
-    console.log(`x is larger`)
-}
+// if (x>y && x>z){
+//     console.log(`x is larger`)
+// }
 
-else if(y>x && y>z){
-    console.log(`y is larger`)
-}
+// else if(y>x && y>z){
+//     console.log(`y is larger`)
+// }
 
-else {
-    console.log(`z is larger`)
-}
+// else {
+//     console.log(`z is larger`)
+// }
+
+
+const x = 45;
+const y = 555;
+const z = 65;
+const result = Math.max(x,y,z)
+
+console.log(`${result} is the largest number`)
