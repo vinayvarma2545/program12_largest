ce7887383366e0356e0c5a8b47b137eb00f64ee2
vinay <vinayvarma2545@gmail.com>
2022-09-21 18:47:38 +0530

    largestAmong

---
+++ largest.js
@@ -0,0 +1,15 @@
+const x = 7;
+const y = 1;
+const z = 55;
+
+if (x>y && x>z){
+    console.log(`x is larger`)
+}
+
+else if(y>x && y>z){
+    console.log(`y is larger`)
+}
+
+else {
+    console.log(`z is larger`)
+}
